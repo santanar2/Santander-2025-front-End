af00ccb8fe7d1c051f05365442a740c744e4e0ca
santanar2 <santana.r2d2@gmail.com>
2025-08-25 11:42:44 -0300

    Adicionando exercícios 3 em Java

---
+++ exercicios 3  - java/2.1 exercicio de fixacao.js
@@ -0,0 +1,14 @@
+//let é a palavra reservada para criar ( variaveis )
+// nome, idade, numero, de telefone, cpf, endereço
+// string = armazenar texto
+// number = armazenar numeros
+// boolean = armazenar valores logicos
+
+
+
+let nomePessoa = "Josicreide Do Pythinho"
+let idade = 115
+let numeroTelefone = "11-984713388"
+let cpf = "123.456.789-10"
+let endereco = "Rua dos bolos, 123 bairro doce"
+let recebePensao = true
