39bbc30a68cdca2d695f0bf3f89ec35a6bfda571
santanar2 <santana.r2d2@gmail.com>
2025-08-26 12:59:56 -0300

    exercicios 4 e 4.1

---
+++ exercicios 3  - java/4.1VetoresEMatrizes.js
@@ -0,0 +1,9 @@
+let pokemon = ["pikachu", "charmander", "bulbasaur"];
+
+let TimePokemon = [
+    ["pikachu", "M", "level 1"],
+    ["charmander", "F", "level 4"], 
+    ["bulbasaur", "M", "level 8"]  
+];
+
+console.log(TimePokemon.length);   
