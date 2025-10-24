a94fc404fc29a862d9ad0e858157ee0a054dadb1
santanar2 <santana.r2d2@gmail.com>
2025-10-24 12:29:20 -0300

    alteração no pokedex, main e um fork js-developer-pokex-main

--- javascript-p.web-6/assets/js/main.js
+++ javascript-p.web-6/assets/js/main.js
@@ -5,12 +5,18 @@ const maxRecords = 151;
 const limit = 10;
 let offset = 0;
 
+// main.js
+
 function convertPokemonToLi(pokemon) {
+    // Nova estrutura para o design: o nome e o número agora estão no topo, 
+    // e o bloco de tipos e a imagem estão na parte inferior do card.
     return `
-        <li class="pokemon ${pokemon.types[0]}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
+        <li class="pokemon ${pokemon.type}">
+            <div class="card-header">
+                <span class="name">${pokemon.name}</span>
+                <span class="number">#${String(pokemon.number).padStart(3, '0')}</span>
+            </div>
+            <div class="card-detail">
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
@@ -19,6 +25,9 @@ function convertPokemonToLi(pokemon) {
         </li>
     `;
 }
+
+// ... o restante do seu main.js continua o mesmo
+
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
