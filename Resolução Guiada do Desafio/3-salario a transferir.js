7e1337f34a848915ef934145bd4e9def03063b5e
santanar2 <santana.r2d2@gmail.com>
2026-01-07 17:00:31 -0300

    conclusão dos desafios

--- Resolução Guiada do Desafio/3-salario a transferir.js
+++ Resolução Guiada do Desafio/3-salario a transferir.js
@@ -9,37 +9,27 @@
 // de R$ 1.100,01 a R$ 2.500,00 = 10,00%
 // maior que R$ 2.500,00 = 15,00%
 
-// exemplo:
-// entrada:
+const readline = require('readline-sync');
 
-// 2000
-// 250
+const valorBruto = parseFloat(readline.question('Digite o valor bruto do salário: '));
+const adicionalBeneficios = parseFloat(readline.question('Digite o valor dos benefícios: '));
 
-// saída:
-// Salário a ser transferido: R$ 2050,00
-
-const prompt = require('./funcoes-auxiliares-ex3.js')();
-const valorBruto = parseFloat(prompt('Digite o valor bruto do salário: '));
-const adicionalBeneficios = parseFloat(prompt('Digite o valor dos benefícios: '));
 let percentualImposto = 0;
+
 if (valorBruto <= 1100) {
     percentualImposto = 0.05;
 } else if (valorBruto <= 2500) {
     percentualImposto = 0.10;
 } else {
     percentualImposto = 0.15;
-}       
+}
+
 const valorImposto = valorBruto * percentualImposto;
 const salarioTransferido = valorBruto - valorImposto + adicionalBeneficios;
+
 console.log(`Salário a ser transferido: R$ ${salarioTransferido.toFixed(2)}`);
-// Cole este comando completo no PowerShell:
-//@"
-//function prompt(question) {
-//    const readline = require('readline-sync');    
-//    return readline.question(question);
-//}
-//module.exports = prompt;
-//"@ | Out-File -FilePath "funcoes-auxiliares-ex3.js" -Encoding utf8
-// node 3-salario a transferir.js
+
+//  node "3-salario a transferir.js"
+
 
 
